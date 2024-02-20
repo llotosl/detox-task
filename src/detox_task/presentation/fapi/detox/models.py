7222c34aed7e1240c553a6b7dd8b7a1f7ec777a5
Oscar Shinov <o.shinov@samolet.ru>
2024-02-20 17:08:46 +0500

    edit detox endpoint

--- src/detox_task/presentation/fapi/detox/models.py
+++ src/detox_task/presentation/fapi/detox/models.py
@@ -2,14 +2,14 @@ from pydantic import BaseModel
 
 
 class CommentModel(BaseModel):
-    text: str
+    texts: tuple[str]
 
 
 class ScoreModel(BaseModel):
-    toxicity: float
-    severe_toxicity: float
-    obscene: float
-    identity_attack: float
-    insult: float
-    threat: float
-    sexual_explicit: float
+    toxicity: list[float]
+    severe_toxicity: list[float]
+    obscene: list[float]
+    identity_attack: list[float]
+    insult: list[float]
+    threat: list[float]
+    sexual_explicit: list[float]
--- src/detox_task/presentation/fapi/detox/router.py
+++ src/detox_task/presentation/fapi/detox/router.py
@@ -10,9 +10,12 @@ from detox_task.presentation.fapi.detox.models import CommentModel, ScoreModel
 detox_router = APIRouter()
 
 
-@lru_cache  # TODO: redis cache and use cache on endpoint instead of
-def model_predict(model: Detoxify, text: str) -> dict:
-    return model.predict(text)
+@lru_cache  # TODO: redis cache and use cache on endpoint instead of this function
+def model_predict(
+    model: Detoxify,
+    texts: tuple[str],
+) -> dict:
+    return model.predict(texts)
 
 
 @detox_router.post("/score-comment", response_model=ScoreModel)
@@ -23,4 +26,4 @@ def score_comment(
         Depends(Stub(Detoxify)),
     ],
 ) -> dict:
-    return model_predict(model, comment.text)
+    return model_predict(model, comment.texts)
